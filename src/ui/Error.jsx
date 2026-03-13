f574caf8a74005b358e42090a6be02b347615fb6
naimatullahofficial <support@naimatullah.com>
2026-03-13 07:46:46 +0500

    error element

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,17 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from 'react-router-dom';
 import React from 'react'
 
 const Error = () => {
-  function NotFound() {
   const navigate = useNavigate();
+  const error = useRouteError();
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{error.data || error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
-}}
+}
 
 export default Error;
