749536307a84959304882dd24907cc8f2937bd1a
naimatullahofficial <support@naimatullah.com>
2026-03-17 05:49:07 +0500

    loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='loader'></div>
+    <div className='absolute bg-slate-200/10 inset-0 backdrop-blur-sm flex items-center justify-center'>
+      <div className='loader'></div>
+    </div>
   )
 }
 
